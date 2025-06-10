ef5275bcacd114bfb6561e09194014808c3fd5c5
boultchi <boultchi@D252-S04>
2025-06-10 09:22:43 +0200

    Class client début fini avec check

--- Client.cs
+++ Client.cs
@@ -1,12 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SAE_201_LOXAM
 {
-    internal class Client
+    public enum Certification
     {
+        Interne = 1,
+        CACES_R486 = 2,
+        CACES_R482 = 3,
+    }
+
+    public class Client
+    {
+        private int numClient;
+        private string nomClient;
+        private string prenomClient;
+
+        public int NumClient
+        {
+            get { return this.numClient; }
+            set { this.numClient = value; }
+        }
+
+        public string NomClient
+        {
+            get { return this.nomClient; }
+            set
+            {
+                if (value.Length > 30)
+                    throw new ArgumentException("Le nom du client ne peut pas dépasser 30 caractères.");
+                this.nomClient = value;
+            }
+        }
+
+        public string PrenomClient
+        {
+            get { return this.prenomClient; }
+            set
+            {
+                if (value.Length > 30)
+                    throw new ArgumentException("Le prénom du client ne peut pas dépasser 30 caractères.");
+                this.prenomClient = value;
+            }
+        }
     }
 }
