de521ddff31d9621e8233e2e93a814f51ab0c7af
cassens <cassens@D252-S06>
2025-06-10 08:34:07 +0200

    Ajoutez des fichiers projet.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SAE_201_LOXAM
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SAE_201_LOXAM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Reserver_Click(object sender, RoutedEventArgs e)
+        {
+            var ReserverFenetre = new Reserver();
+            ReserverFenetre.Show();
+        }
+
+        private void Vérifier_Click(object sender, RoutedEventArgs e)
+        {
+            var VerifierFenetre = new Verifier();
+            VerifierFenetre.Show();
+        }
+
+        private void Consulter_Click(object sender, RoutedEventArgs e)
+        {
+            var ConsulterFenetre = new Consulter();
+            ConsulterFenetre.Show();
+        }
+
+        private void Se_connecter_Click(object sender, RoutedEventArgs e)
+        {
+            var ConnexionFenetre = new Connexion();
+            ConnexionFenetre.Show();
+        }
+    }
+}
