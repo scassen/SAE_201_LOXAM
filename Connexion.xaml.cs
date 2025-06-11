eb9a7eafd0bd9f249b08c31090162dbfd6fd0580
boultchi <boultchi@D252-S04>
2025-06-11 12:42:57 +0200

    Password

--- Connexion.xaml.cs
+++ Connexion.xaml.cs
@@ -30,7 +30,7 @@ namespace SAE_201_LOXAM
         private void Button_Valider_Click(object sender, RoutedEventArgs e)
         {
             string username = identifier_box.Text;
-            string password = mdp_box.Text;
+            string password = mdp_box.Password;
 
             string connString = $"Host=srv-peda-new;Port=5433;Username={username};Password={password};Database=201;Options='-c search_path=MAIN'";
 
