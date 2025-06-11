a7f531b5dcfd9f2ada008bcbff0b73cab082d6e1
boultchi <boultchi@D252-S04>
2025-06-11 11:11:25 +0200

    Reservations

--- Verifier.xaml.cs
+++ Verifier.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -20,9 +23,27 @@ namespace SAE_201_LOXAM
     /// </summary>
     public partial class Verifier : UserControl
     {
+        private ObservableCollection<Reservation> reservations;
+
+        public ObservableCollection<Reservation> Reservations
+        {
+            get => reservations;
+            set
+            {
+                reservations = value;
+                OnPropertyChanged(nameof(Reservations));
+            }
+        }
         public Verifier()
         {
             InitializeComponent();
+            this.DataContext = this;
+
         }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string name) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+     
     }
 }
